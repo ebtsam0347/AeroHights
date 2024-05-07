a3e63906e9a756d8c23e7e279fe5b71dbd28d47b
ebtsam0347 <ebtsam0347@gmail.com>
2024-05-07 16:01:44 +0500

    desingn databse

--- aerohights/base/models.py
+++ aerohights/base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Booking(models.Model):
+    BOOKING_TYPES = [
+        ('Umrah', 'Umrah'),
+        ('Hajj', 'Hajj'),
+        ('Visit', 'Visit'),
+        ('Restaurant', 'Restaurant'),
+        ('Other', 'Other'),
+    ]
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    booking_type = models.CharField(max_length=20, choices=BOOKING_TYPES)
+    date_of_booking = models.DateField()
+    destination = models.CharField(max_length=80)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, null=True, blank=True)
+    duration = models.PositiveIntegerField(default=1, help_text="Duration in days (for bookings like Umrah, Hajj, Visit)")
+
+    def __str__(self):
+        return f"{self.get_booking_type_display()} Booking for {self.client.name}"
